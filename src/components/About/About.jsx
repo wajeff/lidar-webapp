6913129989b8e0675ef191671dc2517b61215559
WaeBae <jrwuoft@gmail.com>
2024-07-03 09:22:10 -0400

    About Component Updates
    Fixed issue where about component css box styling would not cover the entire vertical viewport
    
    Modified index.css and app.module.css to match viewport height

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,12 +3,21 @@ import styles from "./About.module.css";
 
 export const About = () =>{
   return (
-  
+    
     <section className={styles.section}>
-      <div className={styles.squareOne}>
-      </div>
+      <div className={styles.squareOne}/>
+
+
       <div className = {styles.titleContainer}>
         <h1 className = {styles.title}>What is LiDar?</h1>
+        <p>
+        Life Radar, or LiDar for short, is a life tracker web application. 
+
+        <br></br>
+        <br></br>
+
+        LiDar combines a constantly updating visual data structure to help users see which areas of their lives are going well, and which areas need improvement.
+        </p>
         <img className={styles.mathbook} src = "../../../assets/about/mathbook.png"></img>
       </div>
       
